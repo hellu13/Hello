d6f7a80d2a59313e07f24fe23ef0850d27d79090
kyungmo <hellu13@naver.com>
2020-12-01 16:01:49 +0900

    Revise ZoomAct.

---
+++ app/src/main/java/org/techtown/sliverapp/ChooseActivity.java
@@ -0,0 +1,14 @@
+package org.techtown.sliverapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChooseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose);
+    }
+}
